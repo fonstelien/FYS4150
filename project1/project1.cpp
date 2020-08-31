4913e41730a8e82b9cc201e8417a4192ecb4750e
fonstelien <olav.fonstelien@gmail.com>
2020-08-31 17:16:05 +0200

    working solution

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -13,26 +13,31 @@ int main(int argc, char *argv[])
 {
   double start_rng = 0.;
   double end_rng = 1.;
-  int n = (int) 1E1;
+  int n = (int) 1E8;
   double h = (end_rng-start_rng)/(n+1);
 
-  // Init diagonals
-  rowvec a = randu<rowvec>(n);
-  rowvec b = randu<rowvec>(n);
-  rowvec c = randu<rowvec>(n);
+  // // Init diagonals
+  // rowvec a = randu<rowvec>(n);
+  // rowvec b = randu<rowvec>(n);
+  // rowvec c = randu<rowvec>(n);
 
   // Init x,b in Ax = b
   colvec x = linspace<colvec>(start_rng+h, end_rng-h, n);  // Skip range endpoints
-  colvec b_tilde = 100*exp(-10*x);
+  colvec b_tilde = flipud(h*h*100*exp(-10*x));
+  colvec u = flipud(1 - (1 - std::exp((double) -10.))*x - exp(-10*x));
 
-  colvec solution_arma = solve_arma(a, b, c, b_tilde);
-  colvec solution_general = solve_general(a, b, c, b_tilde);
-  (solution_general-solution_arma).print("control general");
+  colvec solution_2n_derivative = solve_2nd_derivative(b_tilde);
 
-  rowvec base = zeros<rowvec>(n);
-  solution_arma = solve_arma(base-1, base+2, base-1, b_tilde);
-  colvec solution_2n_derivative = solve_2nd_derivative(b_tilde);  
-  (solution_2n_derivative-solution_arma).print("control 2nd derivative");
+  cout << "max relative error=" << max(abs((solution_2n_derivative-u)/u)) << endl;
+
+  
+  // colvec solution_arma = solve_arma(a, b, c, b_tilde);
+  // colvec solution_general = solve_general(a, b, c, b_tilde);
+  // (solution_general-solution_arma).print("control general");
+
+  // solution_arma = solve_arma(zeros<rowvec>(n)-1, zeros<rowvec>(n)+2, zeros<rowvec>(n)-1, b_tilde);
+  // colvec solution_2n_derivative = solve_2nd_derivative(b_tilde);  
+  // (solution_2n_derivative-solution_arma).print("control 2nd derivative");
   
   return 0;
 }
@@ -76,7 +81,6 @@ colvec solve_2nd_derivative(colvec b_tilde) {
 
   return solution;
 }
-
 
 colvec solve_arma(rowvec a, rowvec b, rowvec c, colvec b_tilde) {
   int n = b_tilde.n_rows;
