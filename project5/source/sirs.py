a4c4c91a247034e700f61e4797e406e08f095c97
fonstelien <olav.fonstelien@gmail.com>
2020-12-16 15:05:06 +0100

    monte carlo ok

--- project5/source/sirs.py
+++ project5/source/sirs.py
@@ -1,47 +1,60 @@
 import numpy as np
 
 class Sirs:
-    def __init__(self, N0, I0, fa=(lambda t: 4), b=2, c=.5, d=0, dI=0, e=0, f=(lambda t: 0)):
+    def __init__(self, N0, I0, A=0, a=4, b=2, c=.5, d=0, dI=0, e=0, F=0, f=0, fa=None, ff=None):
+        '''Instantiate with initial pop. size and number of infected I0. A,a are the amplitude and average of the seasonal infection rate; b is the recovery rate; c is the immunity loss rate; d is the population death rate from other causes; dI is the insidence death rate; e is the population birth rate; F,f are the amplitude and average of the seasonal vaccination rate; fa,ff accept functions func(t) for infection and vaccination rates.'''
         self.N = N0
         self.S = N0 - I0
         self.I = I0
         self.R = 0
-        self.fa = fa
+        self.fa = fa if fa else (lambda t: A*np.cos(2*np.pi/10*t) + a)
         self.b = b
         self.c = c
         self.d = d
         self.dI = dI
         self.e = e
-        self.f = f
+        self.ff = ff if ff else (lambda t: F*np.cos(2*np.pi/10*t) + f)
         
     def S_dot(self, ti, Si):
-        return self.c*self.R - self.fa(ti)*Si*self.I/self.N - self.d*Si + self.e*self.N - self.f(ti)
+        '''S time derivative.'''
+        return self.c*self.R - self.fa(ti)*Si*self.I/self.N - self.d*Si + self.e*self.N - self.ff(ti)*Si
     
     def I_dot(self, ti, Ii):
+        '''I time derivative.'''
         return self.fa(ti)*self.S*Ii/self.N - (self.b + self.d + self.dI)*Ii
     
     def R_dot(self, ti, Ri):
-        return self.b*self.I - (self.c + self.d)*Ri + self.f(ti)
+        '''R time derivative.'''
+        return self.b*self.I - (self.c + self.d)*Ri + self.ff(ti)*self.S
     
     def N_dot(self, ti, Ni):
+        '''N time derivative.'''
         return self.e*Ni - self.d*self.N - self.dI*self.I
     
     def delta_t(self, ti=0):
+        '''Returns time delta for Monte Carlo simulation.'''
         a = self.fa(ti)
+        f = self.ff(ti)
         return min(4/(a*self.N) if a else 1, 
-                   1/(self.b*self.N), 1/(self.c*self.N), 
-                   1/(self.e) if self.e else 1, 
+                   1/(self.b*self.N),
+                   1/(self.c*self.N), 
                    1/(self.d*self.N) if self.d else 1, 
-                   1/(self.dI*self.N) if self.dI else 1)
+                   1/(self.dI*self.N) if self.dI else 1,
+                   1/(self.e*self.N) if self.e else 1,
+                   1/(f*self.N) if f else 1)
     
     def sir_transitions(self, random_number, dt, ti=0):
+        '''Returns S->I->R->S transitions as (si, ir, rs, sr), where sr is the vaccination transitions.'''
         a = self.fa(ti)
+        f = self.ff(ti)
         si = 1 if random_number < (a*self.S*self.I/self.N)*dt else 0  # S -> I
         ir = 1 if random_number < (self.b*self.I)*dt else 0           # I -> R
         rs = 1 if random_number < (self.c*self.R)*dt else 0           # R -> S
-        return (si, ir, rs)
+        sr = 1 if random_number < (f*self.S)*dt else 0                # S -> R by vaccination
+        return (si, ir, rs, sr)
     
     def vitality_transitions(self, random_number, dt):
+        '''Returns the transitions into and out of life as (bs, sd, idI, idD, rd), where bs are births; sd are deaths among the susceptible; idI are deaths from disease; idD are deaths from other causes among the infected; rd are deaths among the recovering.'''
         bs = 1 if random_number < (self.e*self.N)*dt else 0           # born -> S
         sd = 1 if random_number < (self.d*self.S)*dt else 0           # S -> dead
         idI = 1 if random_number < (self.dI*self.I)*dt else 0         # I -> dead disease
@@ -50,14 +63,18 @@ class Sirs:
         return (bs, sd, idI, idD, rd)
         
     def update(self, new_vals):
+        '''Updates Sirs.S,I,R,N to new_values=(S_new, I_new, R_new, N_new).'''
         S, I, R, N = new_vals
         self.S = S
         self.I = I
         self.R = R
         self.N = N
         
-    def fractions(self):
+    def constant_fractions(self):
+        '''Returns constant fractions in a zero deaths/zero births population.'''
         a = self.fa(0)
+        if a == 0:
+            return (1,0,0)
         s = self.b/a
         i = (1 - self.b/a)/(1 + self.b/self.c)
         r = 1 - s - i
@@ -66,16 +83,19 @@ class Sirs:
 
 class SirsSolver:
     def __init__(self, sirs):
+        '''Instantiate with a Sirs instance.'''
         self.sirs = sirs
         self.fs = {'S':sirs.S_dot, 'I':sirs.I_dot, 'R':sirs.R_dot, 'N':sirs.N_dot}
         self.res = {'S':None, 'I':None, 'R':None, 'N':None}
         self.t = self.h = None
 
     def get_fractions(self):
+        '''Returns the simulation results SirsSolver.res as fractions of dynamical population.'''
         N = self.res['N']
         return (self.res['S']/N, self.res['I']/N, self.res['R']/N)
         
     def _init_run(self, n_iter):
+        '''Initializes the SirsSolver.res data structure. Do this before simulation.'''
         self.res['S'] = np.ndarray(n_iter)
         self.res['I'] = np.ndarray(n_iter)
         self.res['R'] = np.ndarray(n_iter)
@@ -86,6 +106,7 @@ class SirsSolver:
         self.res['N'][0] = self.sirs.N
     
     def _rk4(self, code, i):
+        '''Implementation of RK4'''
         f = self.fs[code]
         yi = self.res[code][i-1]
         ti = self.t[i]
@@ -99,6 +120,7 @@ class SirsSolver:
         return max(0, yi + (k1 + 2*k2 + 2*k3 + k4)/6)
 
     def run_rk4(self, t_max=20, n_iter=1000):
+        '''Run 4th order Runge-Kutta simulation until t_max in n_iter iterations. Result is available in the SirsSolver.res data structure.'''
         self._init_run(n_iter+1)
         self.t = np.linspace(0, t_max, n_iter+1)
         self.h = t_max/n_iter
@@ -112,6 +134,7 @@ class SirsSolver:
     
 
     def run_mc(self, t_max=20, seed=0):
+        '''Run Monte Carlo simulation until t_max. Step length is adaptive. Result is available in the SirsSolver.res data structure.'''
         n_iter = 10000
         self._init_run(n_iter)
         self.t = np.zeros(n_iter)
@@ -132,10 +155,10 @@ class SirsSolver:
                 self.res['N'].resize(i+n_iter, refcheck=False)
             
             random_number = random_numbers[i%n_iter]
-            si, ir, rs = self.sirs.sir_transitions(random_number, dt, ti=ti)
+            si, ir, rs, sr = self.sirs.sir_transitions(random_number, dt, ti=ti)
             bs, sd, idI, idD, rd = self.sirs.vitality_transitions(random_number, dt)
             self.t[i] = ti + dt
-            self.res['S'][i] = self.res['S'][i-1] - si + rs + bs - sd
+            self.res['S'][i] = self.res['S'][i-1] - si + rs + bs - sd - sr
             self.res['I'][i] = self.res['I'][i-1] + si - ir - idI - idD
             self.res['N'][i] = self.res['N'][i-1] + bs - sd - idI - idD - rd
             self.res['R'][i] = self.res['N'][i] - self.res['S'][i] - self.res['I'][i]
